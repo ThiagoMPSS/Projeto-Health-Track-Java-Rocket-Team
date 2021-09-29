7e790b6252facb7ba32a758a688c065ffc698928
Mateus Ferreira <mateusaugustosferreira@hotmail.com>
2021-09-29 20:47:59 -0300

    classe teste dao criada

--- com/healthtrack/Teste.java
+++ com/healthtrack/Teste.java
@@ -1,5 +1,20 @@
 package com.healthtrack;
 
+// import java.util.Scanner;
+
 public class Teste {
+  public static void main(String[] args) {
+    printDados();
+  }
+
+  public static void printDados() {
+    DadosDao dados = new DadosDao();
+
+    Object[] registros = dados.getAll();
+
+    for(int i = 0; i < registros.length; i++){
+      System.out.println(registros[i]);
+    }
+  }
 
 }

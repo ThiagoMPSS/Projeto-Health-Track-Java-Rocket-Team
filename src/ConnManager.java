53ebfec32072b8db0cde133c00d2d9c6e17f66e9
Matheus Sales <mhss2904@gmail.com>
2021-10-21 21:55:09 -0300

    DB_funcionando

---
+++ src/ConnManager.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnManager {
+  private static ConnManager instance;
+  
+  private ConnManager(){}
+
+  public static ConnManager getInstance(){
+    if(instance == null){
+      instance = new ConnManager();
+    }
+    return instance;
+  }
+
+  public Connection getConn(){
+    Connection conn = null;
+
+    try{
+      Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        conn =
+        DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe",
+                                    "system","123");
+
+    System.out.println("Foi foda-se");
+
+    } catch(SQLException e) {
+      System.out.println(e);
+      e.printStackTrace();
+      System.out.println("Não Foi foda-se");
+    } catch(ClassNotFoundException e){
+      System.out.println(e);
+      e.printStackTrace();
+      System.out.println("Não Foi foda-se");
+    }
+    return conn;
+  }
+}
---
+++ src/IDadosDao.java
@@ -0,0 +1,9 @@
+public interface IDadosDao {
+	Object get(long id);
+	Object[] getAll();
+	/*
+	void set(Object informacao);
+	void update(long id, Object informacao);
+	void delete(long id);
+	*/
+}
